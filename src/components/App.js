66a63520eecd059a4058b08a32646c7753a455ec
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-03-29 19:43:12 -0300

    design primeira página

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./Login/Login";
+
 export default function App(){
     return(
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={}></Route>
+                    <Route path="/" element={<Login />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,13 @@
+import * as S from './LoginStyle';
+
 export default function Login(){
     return(
-        
+        <S.Content>
+            <img src='assets/Group 8.svg' alt='logo' />
+            <input placeholder='  usuário' type='text' />
+            <input placeholder='  senha' type='password' />
+            <button>Entrar</button>
+            <a>Não tem uma conta? Cadastre-se!</a>
+        </S.Content>
     )
 }
