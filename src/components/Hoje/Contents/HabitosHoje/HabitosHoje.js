22cca0e3fc167a5696b83cbd23a5d3de3aa39a3d
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-04-03 19:14:48 -0300

    marcar e desmarcar como feito

--- src/components/Hoje/Contents/HabitosHoje/HabitosHoje.js
+++ src/components/Hoje/Contents/HabitosHoje/HabitosHoje.js
@@ -6,6 +6,7 @@ import * as S from './HabitosHojeStyles';
 export default function HabitosHoje(){
     const [obj, setObj] = React.useState([]);
     const context = React.useContext(Context);
+    const [reload, setReload] = React.useState(0);
 
     React.useEffect(() => {
         let token = context.userObj.token;
@@ -15,8 +16,11 @@ export default function HabitosHoje(){
             }
         }
         let promisse = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today', config);
-        promisse.then((response) => setObj(response.data));
-    },[context]);
+        promisse.then((response) => {
+            setObj(response.data)
+            setReload(0);
+        });
+    },[context, reload]);
 
     function setDone(id){
         let token = context.userObj.token;
@@ -25,8 +29,19 @@ export default function HabitosHoje(){
                 'Authorization':`Bearer ${token}`
             }
         }
-        let promisse = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`, config);
-        promisse.then(() => alert('deu certo'))
+        let promisse = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`, {}, config);
+        promisse.then(() => setReload(1))
+    }
+
+    function setUndone(id){
+        let token = context.userObj.token;
+        let config = {
+            headers:{
+                'Authorization':`Bearer ${token}`
+            }
+        }
+        let promisse = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/uncheck`, {}, config);
+        promisse.then(() => setReload(1))
     }
 
     function Habito({ name, current, highest, done, id }){
@@ -37,7 +52,13 @@ export default function HabitosHoje(){
                     <h2>{`Sequencia atual: ${current}`}</h2>
                     <h2>{`Seu recorde: ${highest}`}</h2>
                 </div>
-                <button className={(done)?'selecionado':''} onClick={() => setDone(id)}>marcar</button>
+                <button className={(done)?'selecionado':''} onClick={() => {
+                    if(done){
+                        setUndone(id)
+                    } else {
+                        setDone(id)
+                    }
+                }}>marcar</button>
             </S.Habito>
         )
     }
@@ -52,7 +73,6 @@ export default function HabitosHoje(){
         )
     }
 
-    console.log(obj);
     if (obj.length === 0){
         return(
             <h1>Sem hábitos para hoje</h1>
